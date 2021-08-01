ba1ab13d1d6b3e6be3fa8e8ca3778ce515b98ccd
Akash Srivastava <akashniki@gmail.com>
2021-08-01 16:57:47 +0530

    umi: fod: Refactor and clean it up

--- base.mk
+++ base.mk
@@ -82,7 +82,7 @@ DRM += android.hardware.drm@1.3-service.clearkey
 FASTBOOTD += fastbootd
 
 #FOD
-FOD := vendor.lineage.biometrics.fingerprint.inscreen@1.0-service.xiaomi_kona
+FOD := vendor.lineage.biometrics.fingerprint.inscreen@1.0-service.xiaomi_umi
 
 #framework detect libs
 PRODUCT_PACKAGES += libvndfwk_detect_jni.qti
