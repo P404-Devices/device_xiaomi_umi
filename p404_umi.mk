979a1fb7c6f208dc230e5c33896432f763c53d8f
Akash Srivastava <akashniki@gmail.com>
2022-02-27 17:06:50 +0530

    umi: Welcome Project 404

---
+++ p404_umi.mk
@@ -0,0 +1,38 @@
+#
+# Copyright (C) 2022 Project 404
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from the device configuration.
+$(call inherit-product, device/xiaomi/umi/device.mk)
+
+# Inherit from the 404 configuration.
+$(call inherit-product, vendor/404/configs/common.mk)
+
+# Set Boot Animination Resolution
+TARGET_BOOT_ANIMATION_RES := 1080
+
+PRODUCT_NAME := p404_umi
+PRODUCT_DEVICE := umi
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Mi 10
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+BUILD_FINGERPRINT := Xiaomi/umi/umi:12/SKQ1.211006.001/V13.0.5.1.28.DEV:user/release-keys
